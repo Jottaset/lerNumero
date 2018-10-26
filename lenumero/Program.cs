09dc9909cf74c3652849408e28eb5eb4ca3f1281
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-26 19:39:04 -0400

    le numero

---
+++ lenumero/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lenumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int numero, antecessor, sucessor;
+            numero meunumero = new numero();
+            string texto;
+
+            Console.WriteLine("digite um numero: ");
+            meunumero.defineNumero(Int32.Parse(Console.ReadLine()));
+
+            //sucessor = numero + 1;
+            //antecessor = numero - 1;
+
+            texto = "numero: {0}, sucessor: {1}, antecessor: {2}";
+            Console.WriteLine(texto, meunumero.pegaValorDefinido(), meunumero.sucessorDoNumero(), meunumero.antecessorDoNumero());
+        }
+    }
+}
---
+++ lenumero/numero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace lenumero
+{
+    public class numero
+    {
+        private int meunumero;
+
+        public int pegaValorDefinido(){
+            return meunumero;
+        }
+
+        public void defineNumero(int numeroDaTela)
+        {
+            meunumero = numeroDaTela;
+        }
+
+        public int sucessorDoNumero()
+        {
+            return (meunumero + 1);
+        }
+        public int antecessorDoNumero()
+        {
+            return (meunumero - 1);
+        }
+        public numero()
+        {
+
+        }
+    }
+}
